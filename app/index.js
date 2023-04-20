bfd695197d2b1d2496d70a688ce9f62cc5545227
Siva <siva@hotcode.app>
2023-04-21 03:38:14 +0530

    main.c

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import submission from "./submission/routes.js";
 
+import { spawnSync } from "child_process";
+
 import config from "./configs/config.js";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -11,13 +13,27 @@ app.use(express.urlencoded({ extended: true }));
 
 //Port
 const PORT = config.PORT;
+global.box_id = 1;
 
-app.use("/submission", submission);
+/**
+ * Everytime isolate restarts, there could be some left out files in the container's
+ * isolate directory. Clean them up.
+ */
+const folderCleanUpOptions = ["-rf", "/var/local/lib/isolate/*"];
+const folderCleanUp = spawnSync("rm", folderCleanUpOptions);
+
+console.log(folderCleanUp);
+console.log(folderCleanUp.output[1].toString());
+console.log(folderCleanUp.stderr.toString());
+console.log(folderCleanUp.stdout.toString());
+
 app.get("/", (req, res) => {
     res.status(200).send({
         msg: "ok",
     });
 });
+
+app.use("/submissions", submission);
 
 // Run the server
 app.listen(PORT, async () => {
--- app/submission/routes.js
+++ app/submission/routes.js
@@ -1,16 +1,19 @@
 import express from "express";
-import { spawn, spawnSync } from "child_process";
+import { spawnSync } from "child_process";
+import fs from "fs";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    console.log("/jobs");
-    res.status(200).json({
-        msg: "jobs",
+router.get("/:id", async (req, res) => {
+    console.log("get the status of a submission id");
+    return res.status(200).json({
+        msg: req.params.id,
     });
 });
 
-router.get("/:id", (req, res) => {
-    console.log("get the status of a submission id");
+router.get("/", async (req, res) => {
+    return res.status(200).json({
+        msg: "jobs",
+    });
 });
 
 /**
@@ -25,21 +28,103 @@ router.post("/:id", async (req, res) => {
     /**
      * Initialize a box
      */
-    let boxId = 10;
-    const initOptions = ["--init", "--cg", `--box-id=${boxId}`];
-    const child = spawnSync("isolate", initOptions);
-    res.status(200).json({
-        msg: child,
-    });
+    console.log(global.box_id);
+    if (global.box_id > 999) {
+        global.box_id = 1;
+    } else {
+        global.box_id = global.box_id + 1;
+    }
 
-    // child.on("exit", (code, signal) => {
-    //     console.log(
-    //         "child process exited with " + `code ${code} and signal ${signal}`
-    //     );
-    //     res.status(200).json({
-    //         msg: "job submitted",
-    //     });
-    // });
+    console.log(global.box_id);
+
+    /**
+     * Get input parameters
+     */
+    const language_id = req.body.language_id;
+    const source_code = req.body.source_code;
+
+    /**
+     * Initialize sandbox
+     */
+    const initOptions = ["--init", `--box-id=${global.box_id}`];
+    const init = spawnSync("isolate", initOptions);
+
+    /**
+     * Populate stdin and stdout files
+     */
+    const boxLocation = `/var/local/lib/isolate/${global.box_id}/box`;
+    fs.writeFileSync(`${boxLocation}/stdin.txt`);
+    fs.writeFileSync(`${boxLocation}/stdout.txt`);
+
+    /**
+     * Populate the source code. This is based on the programming language
+     */
+
+    const fileStructure = languageSpecificFileStructure("C");
+    console.log(fileStructure);
+
+    /**
+     * Write the source code file
+     */
+    fs.writeFileSync(
+        `${boxLocation}/${fileStructure.sourceFileName}`,
+        source_code
+    );
+
+    /**
+     * Compile
+     */
+    const compileOptions = [
+        `--box-id=${global.box_id}`,
+        "-p",
+        "--env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        "--stdin=stdin.txt",
+        "--stdout=stdout.txt",
+        "--stderr-to-stdout",
+        "--run",
+        "--",
+        `${fileStructure.compilationCommand}`,
+    ];
+    const compile = spawnSync("isolate", compileOptions);
+
+    /**
+     * Clean up sandbox
+     */
+    // const cleanupOptions = ["--cleanup", `--box-id=${global.box_id}`];
+    // const cleanUp = spawnSync("isolate", cleanupOptions);
+    res.status(200).json({
+        msg: "ok",
+        init: {
+            stdout: compile.stdout.toString(),
+            stderr: compile.stderr.toString(),
+            compile,
+        },
+    });
 });
+
+/**
+ *
+ * @param {*} language_id
+ * @param {*} source_code
+ *
+ * Compilation process is language specific.
+ */
+const languageSpecificFileStructure = (language_id) => {
+    const fileStructure = {
+        sourceFileName: null,
+        compiledFileName: null,
+        outputFileName: null,
+        compilationCommand: null,
+    };
+
+    if (language_id === "C") {
+        fileStructure.sourceFileName = "main.c";
+        fileStructure.outputFileName = "a.out";
+        fileStructure.compilationCommand = `/usr/bin/gcc main.c`;
+        fileStructure.runCommand = `a.out`;
+    }
+
+    return fileStructure;
+};
 
 export default router;
