0148c75e9960ecea49e6285a4688cd21de90f354
Siva <siva@hotcode.app>
2023-04-23 02:19:48 +0530

    try catch block for any errors during code execution

--- app/submission/routes.js
+++ app/submission/routes.js
@@ -27,120 +27,132 @@ router.get("/", async (req, res) => {
  */
 router.post("/:id", async (req, res) => {
     const response = {};
-    /**
-     * Initialize a box
-     */
-    if (global.box_id > 999) {
-        global.box_id = 1;
-    } else {
-        global.box_id = global.box_id + 1;
-    }
-
-    /**
-     * Get input parameters
-     */
-    const language = req.body.language;
-    const source_code = req.body.source_code;
-
-    const time = req.body.time || 5;
-
-    /**
-     * Initialize sandbox
-     */
-    const initOptions = ["--init", `--box-id=${global.box_id}`];
-    const init = spawnSync("isolate", initOptions);
-
-    /**
-     * Populate stdin, stdout and meta files
-     */
-    const boxLocation = `/var/local/lib/isolate/${global.box_id}/box`;
-    fs.writeFileSync(`${boxLocation}/stdin.txt`);
-    fs.writeFileSync(`${boxLocation}/stdout.txt`);
-    fs.writeFileSync(`${boxLocation}/meta.txt`);
-
-    /**
-     * Populate the source code. This is based on the programming language
-     */
-
-    const languageOptions = config.languages[language];
-    const languageDefaultOptions = languageOptions[languageOptions.default];
-    console.log(languageDefaultOptions);
-
-    /**
-     * Write the source code file
-     */
-    fs.writeFileSync(
-        `${boxLocation}/${languageDefaultOptions.sourceFileName}`,
-        source_code
-    );
-    const options = [
-        `--box-id=${global.box_id}`,
-        config.isolate.run.dir,
-        config.isolate.run.processes,
-        config.isolate.run.env,
-        config.isolate.run.stdin,
-        config.isolate.run.stdout,
-        config.isolate.run.stderr_redirect,
-        config.isolate.run.meta,
-        `--time=${time}`,
-        config.isolate.run.run,
-        "--",
-    ];
-
-    if (languageDefaultOptions.compile) {
+    try {
         /**
-         * Compile
+         * Initialize a box
          */
-        const compileOptions = [
-            ...options,
-            `${languageDefaultOptions.compileCommand}`,
+        if (global.box_id > 999) {
+            global.box_id = 1;
+        } else {
+            global.box_id = global.box_id + 1;
+        }
+
+        /**
+         * Get input parameters
+         */
+        const language = req.body.language;
+        const source_code = req.body.source_code;
+
+        const time = req.body.time || 5;
+
+        /**
+         * Initialize sandbox
+         */
+        const initOptions = ["--init", `--box-id=${global.box_id}`];
+        const init = spawnSync("isolate", initOptions);
+
+        /**
+         * Populate stdin, stdout and meta files
+         */
+        const boxLocation = `/var/local/lib/isolate/${global.box_id}/box`;
+        fs.writeFileSync(`${boxLocation}/stdin.txt`);
+        fs.writeFileSync(`${boxLocation}/stdout.txt`);
+        fs.writeFileSync(`${boxLocation}/meta.txt`);
+
+        /**
+         * Populate the source code. This is based on the programming language
+         */
+
+        const languageOptions = config.languages[language];
+        const languageDefaultOptions = languageOptions[languageOptions.default];
+        console.log(languageDefaultOptions);
+
+        /**
+         * Write the source code file
+         */
+        fs.writeFileSync(
+            `${boxLocation}/${languageDefaultOptions.sourceFileName}`,
+            source_code
+        );
+        const options = [
+            `--box-id=${global.box_id}`,
+            config.isolate.run.dir,
+            config.isolate.run.processes,
+            config.isolate.run.env,
+            config.isolate.run.stdin,
+            config.isolate.run.stdout,
+            config.isolate.run.stderr_redirect,
+            config.isolate.run.meta,
+            `--time=${time}`,
+            config.isolate.run.run,
+            "--",
         ];
-        const compile = spawnSync("isolate", compileOptions, { shell: true });
+
+        if (languageDefaultOptions.compile) {
+            /**
+             * Compile
+             */
+            const compileOptions = [
+                ...options,
+                `${languageDefaultOptions.compileCommand}`,
+            ];
+            const compile = spawnSync("isolate", compileOptions, {
+                shell: true,
+            });
+
+            /**
+             * Get the Compile Results
+             */
+            response.compile = {};
+            response.compile.status = compile.status;
+            response.compile.stdin = fs
+                .readFileSync(`${boxLocation}/stdin.txt`)
+                .toString();
+            response.compile.stdout = fs
+                .readFileSync(`${boxLocation}/stdout.txt`)
+                .toString();
+            response.compile.meta = fs
+                .readFileSync(`${boxLocation}/meta.txt`)
+                .toString();
+        }
 
         /**
-         * Get the Compile Results
+         * Run
          */
-        response.compile = {};
-        response.compile.status = compile.status;
-        response.compile.stdin = fs
+        const runOptions = [...options, `${languageDefaultOptions.runCommand}`];
+        const run = spawnSync("isolate", runOptions, { shell: true });
+
+        /**
+         * Get Run Results
+         */
+        response.run = {};
+        response.run.status = run.status;
+        response.run.stdin = fs
             .readFileSync(`${boxLocation}/stdin.txt`)
             .toString();
-        response.compile.stdout = fs
+        response.run.stdout = fs
             .readFileSync(`${boxLocation}/stdout.txt`)
             .toString();
-        response.compile.meta = fs
+
+        response.run.meta = fs
             .readFileSync(`${boxLocation}/meta.txt`)
             .toString();
+        /**
+         * Clean up sandbox
+         */
+        const cleanupOptions = ["--cleanup", `--box-id=${global.box_id}`];
+        const cleanUp = spawnSync("isolate", cleanupOptions, { shell: true });
+
+        res.status(200).json({
+            msg: "ok",
+            response,
+        });
+    } catch (err) {
+        res.status(400).json({
+            msg: "not ok",
+            response,
+        });
     }
-
-    /**
-     * Run
-     */
-    const runOptions = [...options, `${languageDefaultOptions.runCommand}`];
-    const run = spawnSync("isolate", runOptions, { shell: true });
-
-    /**
-     * Get Run Results
-     */
-    response.run = {};
-    response.run.status = run.status;
-    response.run.stdin = fs.readFileSync(`${boxLocation}/stdin.txt`).toString();
-    response.run.stdout = fs
-        .readFileSync(`${boxLocation}/stdout.txt`)
-        .toString();
-
-    response.run.meta = fs.readFileSync(`${boxLocation}/meta.txt`).toString();
-    /**
-     * Clean up sandbox
-     */
-    debugger;
-    // const cleanupOptions = ["--cleanup", `--box-id=${global.box_id}`];
-    // const cleanUp = spawnSync("isolate", cleanupOptions, { shell: true });
-
-    res.status(200).json({
-        msg: "ok",
-        response,
-    });
 });
 
 export default router;
